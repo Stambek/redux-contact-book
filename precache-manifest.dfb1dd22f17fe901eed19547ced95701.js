c7642dda5d612e049f40d0e98cdb87d6c9dbfa3f
Stambek <Stambekm@gmail.com>
2020-09-28 23:07:04 +0600

    Updates

---
+++ precache-manifest.dfb1dd22f17fe901eed19547ced95701.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b865cb7990bdc89e96c6846021ea533",
+    "url": "/redux-contact-book/index.html"
+  },
+  {
+    "revision": "f549cce726ee22531d6f",
+    "url": "/redux-contact-book/static/css/main.37278c17.chunk.css"
+  },
+  {
+    "revision": "16c6b4aa278d00470a74",
+    "url": "/redux-contact-book/static/js/2.fc2087cc.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/redux-contact-book/static/js/2.fc2087cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f549cce726ee22531d6f",
+    "url": "/redux-contact-book/static/js/main.ca268f8e.chunk.js"
+  },
+  {
+    "revision": "40648658aa4c5ddda8cd",
+    "url": "/redux-contact-book/static/js/runtime-main.dd29d345.js"
+  }
+]);
